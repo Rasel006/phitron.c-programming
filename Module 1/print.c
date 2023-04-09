b55609e4a6aa54ba6043396b99da90b07223d1ad
Rasel006 <you@example.com>
2023-04-09 16:47:21 +0600

    c programming

---
+++ Module 1/print.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main(){
+    printf("Assalamualaikum\n");
+    printf("Hello world!\n");
+    printf("In Sha Allah, I am becoming a programmer.");
+    return 0;
+}
--- hello.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-int main(){
-    printf("hello rasel!");
-    return 0;
-}
