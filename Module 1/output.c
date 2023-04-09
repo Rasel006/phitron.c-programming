c0137ae7b9984f4045272b296e796f1f91ce222e
Rasel006 <you@example.com>
2023-04-09 20:25:25 +0600

    c programming

---
+++ Module 1/output.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    //int rahim,karim;
+    //float s;
+    //char c;
+    //scanf("%d%f%c", &rahim, &s, &c);
+    //printf("%d%f%c", rahim, s, c);
+    int a, b;
+    char p;
+    scanf("%d%c %d%c", &a,&p,&b,&p);
+    printf("%d%% %d%%", a, b);
+    return 0;
+}
